3c55cfce107352c6f92a3ff39c90dd05783f1f86
fredBode <fredbode84@gmail.com>
2025-11-25 17:38:51 +0300

    Added about page

---
+++ about/about .js
@@ -0,0 +1,4 @@
+function showAddress() {
+    let address = "123 Main Street, Green Valley, New York,P.O BOX 1484 ,24KM Off the main road";
+    document.getElementById("address").textContent = address;
+}
